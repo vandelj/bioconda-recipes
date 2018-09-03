a237f8e5421f31372e5b0912f55e1b31d1d9b645
Willem <40759303+willemdek11@users.noreply.github.com>
2018-09-03 15:49:08 +0200

    Delete build.sh

--- recipes/plasflow/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# install PlasFlow
-$PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
-
-# install models
-git clone https://github.com/smaegol/PlasFlow.git
-cp -r PlasFlow/models $PREFIX/bin/
