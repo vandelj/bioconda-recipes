cbc6b2dc65dd4803693bca3a68360033b252c394
willemdek11 <willemdek@live.nl>
2018-09-03 15:08:39 +0200

    Update build.sh

--- recipes/bandage/build.sh
+++ recipes/bandage/build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-# install tensorflow version 0.10.0rc0
-pip install --no-deps --ignore-installed --no-cache-dir  tensorflow/tensorflow-0.10.0rc0-cp35-cp35m-linux_x86_64.whl
-# install PlasFlow
-$PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
-# copy models
-cp -r PlasFlow/models $PREFIX/bin/
+
+# Set the environment variable to specify that you will be using Qt 5, not Qt 4
+export QT_SELECT=5
+
+# cd to location of source
+cd $SRC_DIR/
+
+# Run qmake to generate a Makefile
+qmake
+
+# Build the program
+make
+
