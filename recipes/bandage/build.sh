4e408102dab5bfe45390ff691598e7cccc818e53
willemdek11 <willemdek@live.nl>
2018-09-05 15:31:46 +0200

    update files

--- recipes/bandage/build.sh
+++ recipes/bandage/build.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Set the environment variable to specify that you will be using Qt 5, not Qt 4
-export QT_SELECT=5
-
-# cd to location of source
-cd $SRC_DIR/
-
 # Run qmake to generate a Makefile
 qmake
 
 # Build the program
 make
 
+# Install
+make install
